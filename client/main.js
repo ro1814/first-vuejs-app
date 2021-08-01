76c85cc4c3b91e20e2cb3779e9d429782cf626b4
ro1814 <rnsm14@gmail.com>
2021-08-01 18:28:05 +0200

    first commit. Starting to learn Vue.JS :)

---
+++ client/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el:'#app',
+    data: {
+        product: 'Socks',
+        description: 'The best socks on earth',
+        image: './assets/vmSocks-green.jpg',
+        link: 'https://www.happysocks.com/'
+    }
+})
